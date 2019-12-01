53a73c85393305de9e9b1d0bff7af52254b757cf
Jay Q <13415145594@163.com>
2019-12-01 19:42:55 +0800

    fix create_tomcat_centos_images.sh 0f /etc/profile

--- docker/create_tomcat_centos_images.sh
+++ docker/create_tomcat_centos_images.sh
@@ -11,7 +11,12 @@ echo "正在配置环境变量中"
 ## export JRE_HOME=/usr/local/jdk1.8.0_161/jre和export JAVA_HOME=/usr/local/jdk1.8.0_161
 ##必须放在setclasspath.sh前面，否则会报错找不到jdk和jre
 echo "*******************************************"
-echo -e "JAVA_HOME=/usr/local/jdk1.8.0_161\nexport PATH=$JAVA_HOME/bin:$PATH">>/etc/profile
+echo -e "JAVA_HOME=/usr/local/jdk1.8.0_161">>/etc/profile
+echo "export CLASSPATH=.:${JAVA_HOME}/jre/lib/rt.jar:${JAVA_HOME}/lib/dt.jar:${JAVA_HOME}/lib/tools.jar">>/etc/profile
+echo "export PATH=$PATH:${JAVA_HOME}/bin">>/etc/profile
+
+export 
+export 
 sed -i '1 a\export JAVA_HOME=/usr/local/jdk1.8.0_161' /usr/local/apache-tomcat-7.0.47/bin/setclasspath.sh
 sed -i '1 a\export JRE_HOME=/usr/local/jdk1.8.0_161/jre' /usr/local/apache-tomcat-7.0.47/bin/setclasspath.sh
 source /etc/profile
